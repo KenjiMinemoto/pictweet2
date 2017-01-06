68863b3250d777a43ab395efb4dae7a6f5f4f779
嶺本賢志 <13e0578@tku.ac.jp>
2017-01-06 10:14:03 +0900

    復活

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105065701) do
+ActiveRecord::Schema.define(version: 20170106003859) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "name",       limit: 255

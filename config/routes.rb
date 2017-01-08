4b29fb9b3bca60536d78d7884214aa01a15e58f9
嶺本賢志 <13e0578@tku.ac.jp>
2017-01-09 08:31:14 +0900

    削除機能の実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
     root  'tweets#index'
     get   'tweets'      =>  'tweets#index'
     get   'tweets/new'  =>  'tweets#new'
-    post  '/tweets'     =>  'tweets#create'
-    get   'users/:id'   =>  'users#show'
+    post  'tweets'      =>  'tweets#create'
+    delete  'tweets/:id'  => 'tweets#destroy'
+    get   'users/:id'  =>  'users#show'
   end

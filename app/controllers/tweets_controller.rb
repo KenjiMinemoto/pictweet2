8db8e423f5eea74b68a52812b1473f7a1bd8ffc0
嶺本賢志 <13e0578@tku.ac.jp>
2017-01-09 09:49:49 +0900

    更新後のコントローラの実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -24,6 +24,12 @@
       @tweet = Tweet.find(params[:id])
     end
 
+    def update
+      tweet = Tweet.find(params[:id])
+      if tweet.user_id == current_user.id
+        tweet.update(tweet_params)
+    end
+
     private
     def tweet_params
       params.permit(:image, :text)
